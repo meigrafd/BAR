24f05b4273f0eebb5af83e0fb98dd02b2bf34e66
meigrafd <krunzer@gmail.com>
2024-08-12 15:19:32 +0200

    geo and mex workaround

--- Scavengers_Mod/tweakdefs3.lua
+++ Scavengers_Mod/tweakdefs3.lua
@@ -6,11 +6,12 @@ local toEvolve = {
             {from="armadvsol", fromDesc="Advanced Solar Collector", to="armwint2", toDesc="Advanced Wind Turbine", timer=240},
             {from="armwint2", fromDesc="Fusion Reactor", to="armfus", toDesc="Advanced Fusion Reactor", timer=360},
             {from="armfus", fromDesc="Fusion Reactor", to="armafus", toDesc="Advanced Fusion Reactor", timer=360},
-            -- sadly geo and mex throws errors, so disabled for fairplay till devs fix this
-            --{from="armgeo", fromDesc="Geothermal Powerplant", to="armageo", toDesc="Advanced Geothermal Powerplant", timer=360},
+            {from="armgeo", fromDesc="Geothermal Powerplant", to="armfakegeo", toDesc="-Workaround Fake Geo-", timer=360},
+            {from="armfakegeo", fromDesc="-Workaround Fake Geo-", to="armageo", toDesc="Advanced Geothermal Powerplant", timer=2},
             {from="armtide", fromDesc="Tidal Generator", to="armuwfus", toDesc="Naval Fusion Reactor", timer=300},
             -- Metal
-            --{from="armmex", fromDesc="Metal Extractor", to="armmoho", toDesc="Advanced Metal Extractor", timer=360},
+            {from="armmex", fromDesc="Metal Extractor", to="armfakemex", toDesc="-Workaround Fake Mex-", timer=360},
+            {from="armfakemex", fromDesc="-Workaround Fake Mex-", to="armmoho", toDesc="Advanced Metal Extractor", timer=2},
             -- Wall
             {from="armdrag", fromDesc="Dragon's Teeth", to="armfort", toDesc="Fortification Wall", timer=360},
             -- other buildings
@@ -24,9 +25,11 @@ local toEvolve = {
             {from="coradvsol", fromDesc="Advanced Solar Collector", to="corwint2", toDesc="Advanced Wind Turbine", timer=240},
             {from="corwint2", fromDesc="Advanced Wind Turbine", to="corfus", toDesc="Fusion Reactor", timer=300},
             {from="corfus", fromDesc="Fusion Reactor", to="corafus", toDesc="Advanced Fusion Reactor", timer=360},
-            --{from="corgeo", fromDesc="Geothermal Powerplant", to="corageo", toDesc="Advanced Geothermal Powerplant", timer=360},
+            {from="corgeo", fromDesc="Geothermal Powerplant", to="corfakegeo", toDesc="-Workaround Fake Geo-", timer=360},
+            {from="corfakegeo", fromDesc="-Workaround Fake Geo-", to="corageo", toDesc="Advanced Geothermal Powerplant", timer=2},
             {from="cortide", fromDesc="Tidal Generator", to="coruwfus", toDesc="Naval Fusion Reactor", timer=300},
-            --{from="cormex", fromDesc="Metal Extractor", to="cormoho", toDesc="Advanced Metal Extractor", timer=360},
+            {from="cormex", fromDesc="Metal Extractor", to="corfakemex", toDesc="-Workaround Fake Mex-", timer=360},
+            {from="corfakemex", fromDesc="-Workaround Fake Mex-", to="cormoho", toDesc="Advanced Metal Extractor", timer=2},
             {from="cordrag", fromDesc="Dragon's Teeth", to="corfort", toDesc="Fortification Wall", timer=360},
             {from="cornanotc", fromDesc="Construction Turret", to="cornanotct2", toDesc="Advanced Construction Turret", timer=120},
             {from="cornanotcplat", fromDesc="Naval Construction Turret", to="cornanotc2plat", toDesc="Advanced Naval Construction Turret", timer=120},
@@ -38,10 +41,26 @@ local toEvolve = {
             -- legion uses cortex after legadvsol
             {from="legadvsol", fromDesc="Advanced Solar Collector", to="corwint2", toDesc="Advanced Wind Turbine", timer=240},
             {from="legtide", fromDesc="Tidal Generator", to="coruwfus", toDesc="Naval Fusion Reactor", timer=300},
-            --{from="legmex", fromDesc="Metal Extractor", to="armmoho", toDesc="Advanced Metal Extractor", timer=360},
+            {from="legmex", fromDesc="Metal Extractor", to="legfakemex", toDesc="-Workaround Fake Mex-", timer=360},
+            {from="legfakemex", fromDesc="-Workaround Fake Mex-", to="legmoho", toDesc="Advanced Metal Extractor", timer=2},
             {from="legdrag", fromDesc="Dragon's Teeth", to="corfort", toDesc="Fortification Wall", timer=360},
         },
 }
+
+-- create fake mex and geo as workaround of current game-error.
+UnitDefs["armfakemex"] = table.copy(UnitDefs["armmex"])
+UnitDefs["armfakemex"].extractsmetal = 0
+UnitDefs["armfakegeo"] = table.copy(UnitDefs["armgeo"])
+UnitDefs["armfakegeo"].customparams.geothermal = nil
+UnitDefs["corfakemex"] = table.copy(UnitDefs["cormex"])
+UnitDefs["corfakemex"].extractsmetal = 0
+UnitDefs["corfakegeo"] = table.copy(UnitDefs["corgeo"])
+UnitDefs["corfakegeo"].customparams.geothermal = nil
+UnitDefs["legfakemex"] = table.copy(UnitDefs["legmex"])
+UnitDefs["legfakemex"].extractsmetal = 0
+--UnitDefs["legfakegeo"] = table.copy(UnitDefs["leggeo"]
+--UnitDefs["legfakegeo"].customparams.geothermal = nil
+
 for unitName, unitData in pairs(UnitDefs) do
     faction = string.sub(unitName, 1, 3)
     for evoFaction,_ in pairs(toEvolve) do
